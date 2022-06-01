b5f9fe54864046735018d4a28787c8708933fcdb
Matthew Downs <downs.matt@gmail.com>
2022-06-01 16:24:04 -0700

    Use .env.local instead of inline constants for rapid api details

--- services/queryGenius.js
+++ services/queryGenius.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
-const GENIUS_SEARCH_URL = "https://genius.p.rapidapi.com/search";
-const RAPID_API_KEY = "aeba5ecb0cmsh63bd8d2cab240d8p11745bjsn7d438afe4288";
-const RAPID_API_HOST = "genius.p.rapidapi.com";
-
 async function queryGenius(query) {
     if (query === "") return [];
 
     const options = {
         method: "GET",
-        url: GENIUS_SEARCH_URL,
+        url: process.env.NEXT_PUBLIC_GENIUS_SEARCH_URL,
         params: { q: query },
         headers: {
-            "X-RapidAPI-Host": RAPID_API_HOST,
-            "X-RapidAPI-Key": RAPID_API_KEY,
+            "X-RapidAPI-Host": process.env.NEXT_PUBLIC_RAPID_API_HOST,
+            "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
         },
     };
 
